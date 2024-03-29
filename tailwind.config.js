9235a1264c2f04fb885bbd2b07a7a6b2b5cb4eb8
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-29 16:06:28 -0300

    Set custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,21 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode:'selector',
   theme: {
+    colors:{
+      "primary": "#96031a",
+      "secondary": "#ef6f6c",
+      "tertiary": "#6D676E",
+      "black": "#1B1B1E",
+      "white": "#FBFFFE"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }
     },
   },
   plugins: [],
