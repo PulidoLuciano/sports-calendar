adedfc7a1bdf95d451fd678f6c2c45b5fdef3948
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-04-06 14:59:00 -0300

    Add key to team items

--- src/app/[leagueId]/page.js
+++ src/app/[leagueId]/page.js
@@ -14,7 +14,7 @@ export default async function Page({params}){
                 <h1>{league.name}</h1>
             </header>
             <section className="p-3 mt-6 mb-6 lg:p-0 mx-auto max-w-4xl">
-                {teams.map((team) => <TeamItem name={team.name} logo={team.logo}/>)}
+                {teams.map((team) => <TeamItem key={team.name} name={team.name} logo={team.logo}/>)}
             </section>
         </>
     )
