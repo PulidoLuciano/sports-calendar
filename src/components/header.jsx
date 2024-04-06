f1530290d589a3397abd84e5162e4b94db78c0eb
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-04-06 13:04:44 -0300

    Sticky to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,7 +19,7 @@ export default function Header(){
     }
     
     return(
-        <header className="bg-primary md:px-16 p-3 shadow-sm shadow-tertiary flex items-center justify-between">
+        <header className="bg-primary md:px-16 p-3 shadow-sm shadow-tertiary flex items-center justify-between sticky top-0 left-0 z-50">
             <Link href={"/"} className="text-white font-extrabold select-none">SportsCalendar</Link>
             <div className="flex gap-4">
                 <button onClick={changeTheme}><img src={(themeContext.theme == "") ? "./lightmode.svg" : "./darkmode.svg"} alt={`${themeContext.theme} theme logo`} className="invert"/></button>
