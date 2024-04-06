eaac5a3d4cd97bc8acebf4fc0a7f915e8a18a08b
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-04-06 13:05:01 -0300

    Add translations for tutorial

--- src/utils/languages/english.js
+++ src/utils/languages/english.js
@@ -4,7 +4,15 @@ export default{
     check: "Check the available leagues ",
     below: "below",
     notKnow: "Don't you know how to suscribe to your team's calendar?",
-    watch: "Watch this",
+    watch: "Read this",
     tutorial: "tutorial",
-    available: "Available leagues"
+    available: "Available leagues",
+    howTo: "How to suscribe to a team's calendar",
+    tutorial_one: "SportsCalendar is a page that gives you calendars that you can add to your Google calendar. These have all the matches of your favorite teams already scheduled. The procedure to subscribe to a team's calendar is very simple and we explain it to you below:",
+    step_one: "Choose your team's league on the main page, then click the 'Copy' button to copy the calendar link to your device's clipboard.",
+    step_two: "Go to the google calendar page.",
+    step_three: "On the Google calendar page you will see on the lower left side a section called 'Other calendars' with a '+' next to it. Click on the '+'",
+    step_four: "A list of options will open, click on the 'From URL' option.",
+    step_five: "There you must paste the link (with right click and paste or Ctrl+V) and then press the 'Add calendar' button",
+    tutorial_two: "Congratulations! You are subscribed to your team calendar and you never will lost a game again."
 }
--- src/utils/languages/spanish.js
+++ src/utils/languages/spanish.js
@@ -4,8 +4,15 @@ export default {
     check: "Mira las ligas disponibles ",
     below: "abajo",
     notKnow: "¿No sabes cómo suscribirte al calendario de tu equipo?",
-    watch: "Mira este",
+    watch: "Lee este",
     tutorial: "tutorial",
     available: "Ligas disponibles",
-    
+    howTo: "Como suscribirte al calendario de un equipo",
+    tutorial_one: "SportsCalendar es una página que te brinda calendarios que puedes añadir a tu calendario de google. Estos cuentan con todos los partidos de tus equipos favoritos ya programados. El procedimiento para suscribirse al calendario de un equipo es muy sencillo y te lo explicamos a continuación:",
+    step_one: "Elige la liga de tu equipo en la página principal, luego haz click en el botón 'Copiar' para copiar el link del calendario en el portapapeles de tu dispositivo.",
+    step_two: "Ve a la página de google calendar.",
+    step_three: "En la página de google calendar verás al lado izquierdo inferior una sección llamada 'Otros calendarios' con un '+' al lado. Haz click sobre el '+'",
+    step_four: "Se abrirá una lista de opciones, haz click sobre la opción 'Desde URL'.",
+    step_five: "Allí deberás pegar el link (con click derecho y pegar o Ctrl+V) y luego pulsar el botón 'Añadir calendario'",
+    tutorial_two: "¡Felicidades! Estás suscrito al calendario de tu equipo y nunca más te perderás un partido."
 }
