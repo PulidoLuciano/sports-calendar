fdaf2da7adf50e16859b43436e463e47bf2d19b9
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-04-20 01:00:25 -0300

    Add error handling to suscribe route

--- src/app/api/suscribe/route.js
+++ src/app/api/suscribe/route.js
@@ -2,13 +2,18 @@ import { suscribe } from "@/app/lib/actions";
 import { sql } from "@vercel/postgres";
 
 export async function GET(request){
-    const { searchParams } = new URL(request.url);
-    let userEmail = searchParams.get("user");
-    let teamId = searchParams.get("team");
-    let user = await sql`SELECT id FROM users WHERE email=${userEmail}`;
-    let suscribe = await sql`SELECT id FROM suscribes WHERE teamid=${teamId} AND userid=${user.rows[0].id}`;
-
-    return Response.json((suscribe.rowCount > 0));
+    try{
+        const { searchParams } = new URL(request.url);
+        let userEmail = searchParams.get("user");
+        let teamId = searchParams.get("team");
+        let user = await sql`SELECT id FROM users WHERE email=${userEmail}`;
+        let suscribes = await sql`SELECT id FROM suscribes WHERE teamid=${teamId} AND userid=${user.rows[0].id}`;
+    
+        return Response.json((suscribes.rowCount > 0));
+    }catch(e){
+        console.log(e);
+        return Response.json(e);
+    }
 }
 
 export async function POST(request){
